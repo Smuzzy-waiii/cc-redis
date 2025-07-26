7a51fec8f3eb341b3144f87b99d249f514e15df6
Smuzzy-waiii <smaran.jawalkar@gmail.com>
2025-07-26 18:39:03 +0530

    fixx conn

--- app/main.py
+++ app/main.py
@@ -9,9 +9,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, _ = server_socket.accept() # wait for client
-    connection.sendall(b"+PONG\r\n")
-
-
+    conn.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
